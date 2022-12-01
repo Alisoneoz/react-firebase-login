87ece115840a592c42620757b7d01fa19729f20e
Alison Estephany <alisonestephanyoz@gmail.com>
2022-11-30 21:01:12 -0400

    fixed typos at the individual routes' elements

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { Register } from './components/Register'
 function App() {
   return (
     <Routes>
-      <Route path='/' element={ Home }></Route>
-      <Route path='/login' element={ Login }></Route>
-      <Route path='/register' element={Register}></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/login' element={ <Login /> }></Route>
+      <Route path='/register' element={<Register />}></Route>
     </Routes>
   );
 }
