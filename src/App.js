cad6868249d7efc9174a474126a3390065dd992c
Alison Estephany <alisonestephanyoz@gmail.com>
2022-11-30 22:22:27 -0400

    created and imported Context provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
 import { Login } from './components/Login'
 import { Register } from './components/Register'
+import { AuthProvider } from "./components/context/authContext"
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/login' element={ <Login /> }></Route>
-      <Route path='/register' element={<Register />}></Route>
-    </Routes>
+    <div className='bg-fuchsia-900 text-white h-screen'>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/login' element={ <Login /> }></Route>
+        <Route path='/register' element={<Register />}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,19 @@
 export const Login = () => {
+  
+  const [ user, setUser] = useState({
+    email:'',
+    password:''
+  })
+
   return (
-    <div>Login</div>
+    <div>
+      
+      <form>
+        <input type='email' name='email' id='email'></input>
+
+        <input type='password' name='password'  id='password'></input>
+      </form>
+
+    </div>
   )
 }
