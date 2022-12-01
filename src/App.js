1918db6d2be36b25c2035025d0aee74910800ef5
Alison Estephany <alisonestephanyoz@gmail.com>
2022-11-30 20:41:05 -0400

    Installed react-router-dom and created some of the pages for the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-  <h1 className="text-3xl">
-    Hello world!
-  </h1>
+    <Routes>
+      <Route></Route>
+    </Routes>
   );
 }
 
